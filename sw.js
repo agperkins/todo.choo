b4abccffb75ea4eb5df71608dd874d17b1a18c09
agperkins <alexandrasaff@gmail.com>
2018-03-25 18:45:21 -0500

    Format code

--- sw.js
+++ sw.js
@@ -5,24 +5,32 @@ var URLS = process.env.FILE_LIST
 
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
-  e.respondWith(self.caches.match(e.request).then(function (request) {
-    if (request) return request
-    else return self.fetch(e.request)
-  }))
+  e.respondWith(
+    self.caches.match(e.request).then(function (request) {
+      if (request) return request
+      else return self.fetch(e.request)
+    })
+  )
 })
 
 // Register worker
 self.addEventListener('install', function (e) {
-  e.waitUntil(self.caches.open(VERSION).then(function (cache) {
-    return cache.addAll(URLS)
-  }))
+  e.waitUntil(
+    self.caches.open(VERSION).then(function (cache) {
+      return cache.addAll(URLS)
+    })
+  )
 })
 
 // Remove outdated resources
 self.addEventListener('activate', function (e) {
-  e.waitUntil(self.caches.keys().then(function (keyList) {
-    return Promise.all(keyList.map(function (key, i) {
-      if (keyList[i] !== VERSION) return self.caches.delete(keyList[i])
-    }))
-  }))
+  e.waitUntil(
+    self.caches.keys().then(function (keyList) {
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (keyList[i] !== VERSION) return self.caches.delete(keyList[i])
+        })
+      )
+    })
+  )
 })
