60e281a8b17ac99b1743c27533fa78cd07caa3f0
agperkins <alexandrasaff@gmail.com>
2018-03-25 18:45:57 -0500

    Add new click emitters

--- stores/clicks.js
+++ stores/clicks.js
@@ -2,10 +2,23 @@ module.exports = store
 
 function store (state, emitter) {
   state.totalClicks = 0
+  state.talkOfTheTown = ''
 
   emitter.on('DOMContentLoaded', function () {
+    emitter.on('clicks:reset', function () {
+      state.totalClicks = 0
+      emitter.emit(state.events.RENDER)
+    })
     emitter.on('clicks:add', function (count) {
       state.totalClicks += count
+      emitter.emit(state.events.RENDER)
+    })
+    emitter.on('clicks:subtract', function (count) {
+      state.totalClicks -= count
+      emitter.emit(state.events.RENDER)
+    })
+    emitter.on('mouth:talk', function (text) {
+      state.talkOfTheTown = text
       emitter.emit(state.events.RENDER)
     })
   })
