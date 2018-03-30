7a9fac0d2a240ca0e9b39c399b6fedd615c8a01c
agperkins <alexandrasaff@gmail.com>
2018-03-30 11:10:47 -0500

    Add page Clicky

---
+++ views/clicky.js
@@ -0,0 +1,58 @@
+var html = require('choo/html')
+var wrapper = require('../components/wrapper')
+
+module.exports = wrapper(view)
+
+var TITLE = 'Clicky'
+
+function view (state, emit) {
+  if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
+  return html`
+      <main class="sans-serif pa3">
+        <h1>Clicky</h1>
+        <p>Click around to see my progress</p>
+      </main>
+      <section class="fl mw6 w-third-l pa3 sectionWidth">
+        <h2 class="f-headline lh-solid">Clicky</h2>
+          <p> Number of clicks stored: ${state.totalClicks}</p>
+          <p>${state.talkOfTheTown}</p>
+
+          <div class="buttonWrap">
+            <button class="button"
+              onclick=${handleSubtract}>
+              down
+            </button>
+
+            <button class="button"
+              onclick=${handleReset}>
+              reset
+            </button>
+
+            <button class="button"
+              onclick=${handleClick}>
+              up
+            </button>
+
+            <button class="button"
+              onclick=${talk}>
+              sentence of the day
+            </button>
+          </div>
+
+        </section>
+      </main>
+    </body>
+  `
+  function handleReset () {
+    emit('clicks:reset')
+  }
+  function handleClick () {
+    emit('clicks:add', 1)
+  }
+  function handleSubtract () {
+    emit('clicks:subtract', 1)
+  }
+  function talk () {
+    emit('mouth:talk', 'This is different')
+  }
+}
