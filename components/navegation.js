b14e2cb773db82ace8ec512e300cf8208eba37a6
agperkins <alexandrasaff@gmail.com>
2018-03-30 11:10:28 -0500

    Add navegation component

---
+++ components/navegation.js
@@ -0,0 +1,12 @@
+var html = require('choo/html')
+
+module.exports = navigation
+
+function navigation () {
+  return html`
+      <nav class="pa3">
+        <a class="navlink" href="/">Main</a> |
+        <a class="navlink" href="/">Clicky</a>
+        </nav>
+    `
+}
