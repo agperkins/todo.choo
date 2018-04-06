25ba04df1b09279be14dc97b38f901303444bd35
agperkins <alexandrasaff@gmail.com>
2018-04-06 15:28:26 -0500

    Format code

--- sw.js
+++ sw.js
@@ -4,9 +4,9 @@ var VERSION = require('./package.json').version
 var URLS = process.env.FILE_LIST
 
 // Respond with cached resources
-self.addEventListener('fetch', function (e) {
+self.addEventListener('fetch', function(e) {
   e.respondWith(
-    self.caches.match(e.request).then(function (request) {
+    self.caches.match(e.request).then(function(request) {
       if (request) return request
       else return self.fetch(e.request)
     })
@@ -14,20 +14,20 @@ self.addEventListener('fetch', function (e) {
 })
 
 // Register worker
-self.addEventListener('install', function (e) {
+self.addEventListener('install', function(e) {
   e.waitUntil(
-    self.caches.open(VERSION).then(function (cache) {
+    self.caches.open(VERSION).then(function(cache) {
       return cache.addAll(URLS)
     })
   )
 })
 
 // Remove outdated resources
-self.addEventListener('activate', function (e) {
+self.addEventListener('activate', function(e) {
   e.waitUntil(
-    self.caches.keys().then(function (keyList) {
+    self.caches.keys().then(function(keyList) {
       return Promise.all(
-        keyList.map(function (key, i) {
+        keyList.map(function(key, i) {
           if (keyList[i] !== VERSION) return self.caches.delete(keyList[i])
         })
       )
