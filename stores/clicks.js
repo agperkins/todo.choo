0b266300ae8f19557c25206f7720b077183c3abf
agperkins <alexandrasaff@gmail.com>
2018-04-06 15:29:32 -0500

    Format code

--- stores/clicks.js
+++ stores/clicks.js
@@ -1,32 +1,32 @@
 module.exports = store
 
-function store (state, emitter) {
+function store(state, emitter) {
   state.totalClicks = 0
   state.talkOfTheTown = ''
   state.todoItems = []
 
-  emitter.on('DOMContentLoaded', function () {
-    emitter.on('clicks:reset', function () {
+  emitter.on('DOMContentLoaded', function() {
+    emitter.on('clicks:reset', function() {
       state.totalClicks = 0
       emitter.emit(state.events.RENDER)
     })
-    emitter.on('clicks:add', function (count) {
+    emitter.on('clicks:add', function(count) {
       state.totalClicks += count
       emitter.emit(state.events.RENDER)
     })
-    emitter.on('clicks:subtract', function (count) {
+    emitter.on('clicks:subtract', function(count) {
       state.totalClicks -= count
       emitter.emit(state.events.RENDER)
     })
-    emitter.on('mouth:talk', function (text) {
+    emitter.on('mouth:talk', function(text) {
       state.talkOfTheTown = text
       emitter.emit(state.events.RENDER)
     })
-    emitter.on('todo:Add', function (text) {
+    emitter.on('todo:Add', function(text) {
       state.todoItems.push(text)
       emitter.emit(state.events.RENDER)
     })
-    emitter.on('todo:delete', function (index) {
+    emitter.on('todo:delete', function(index) {
       state.todoItems.splice(index, 1)
       emitter.emit(state.events.RENDER)
     })
